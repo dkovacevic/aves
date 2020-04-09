675edca5b25449e87c6d15469867a9ee2886e973
dkovacevic <dejankov@gmail.com>
2020-04-09 21:31:33 +0200

    Signatures: error handling

--- src/main/java/com/aves/server/resource/SignatureResource.java
+++ src/main/java/com/aves/server/resource/SignatureResource.java
@@ -5,14 +5,12 @@ import com.aves.server.clients.SwisscomClient;
 import com.aves.server.model.ErrorMessage;
 import com.aves.server.model.User;
 import com.aves.server.tools.Logger;
-import com.aves.server.tools.Util;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import org.jdbi.v3.core.Jdbi;
 
-import javax.annotation.Nullable;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
@@ -20,9 +18,6 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Base64;
 import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -47,13 +42,6 @@ public class SignatureResource {
     @Authorization("Bearer")
     public Response request(@Context ContainerRequestContext context, @ApiParam @Valid SignRequest request) {
         try {
-            final SignResponse dummySigResponse = getDummySigResponse();
-            if (dummySigResponse != null) {
-                return Response.
-                        ok(dummySigResponse).
-                        build();
-            }
-
             UUID signer = (UUID) context.getProperty("zuid");
 
             if (rateLimit(signer))
@@ -71,7 +59,11 @@ public class SignatureResource {
 
             final SwisscomClient.OptionalOutputs optionalOutputs = signResponse.optionalOutputs;
 
-            if (optionalOutputs == null) {
+            if (optionalOutputs == null || optionalOutputs.stepUpAuthorisationInfo == null) {
+                Logger.warning("SignatureResource.request : missing data from AIS. userId: %s, phone: %s, error: %s",
+                        user.id,
+                        user.phone,
+                        signResponse.result.minor);
                 return Response.
                         ok(new ErrorMessage(signResponse.result.minor, 400, "ais-error")).
                         status(400).
@@ -80,14 +72,20 @@ public class SignatureResource {
 
             SignResponse result = new SignResponse();
             result.responseId = optionalOutputs.responseId;
-            if (optionalOutputs.stepUpAuthorisationInfo != null)
-                result.consentURL = optionalOutputs.stepUpAuthorisationInfo.result.url;
+            result.consentURL = optionalOutputs.stepUpAuthorisationInfo.result.url;
+
+            if (result.consentURL == null) {
+                Logger.warning("SignatureResource.request : consentURL is null. userId: %s, phone: %s", user.id, user.phone);
+                return Response.
+                        ok(new ErrorMessage("Missing consent URL", 400, "ais-error")).
+                        status(400).
+                        build();
+            }
 
             return Response.
                     ok(result).
                     build();
         } catch (Exception e) {
-            //e.printStackTrace();
             Logger.error("SignatureResource.request : %s", e);
             return Response
                     .ok(new ErrorMessage(e.getMessage()))
@@ -102,14 +100,6 @@ public class SignatureResource {
     @Authorization("Bearer")
     public Response pending(@Context ContainerRequestContext context, @PathParam("responseId") UUID responseId) {
         try {
-            Signature dummy = getDummySignature();
-
-            if (dummy != null) {
-                return Response.
-                        ok(dummy).
-                        build();
-            }
-
             SwisscomClient.SignResponse signResponse = swisscomClient.pending(responseId);
 
             if (signResponse == null || signResponse.signature == null) {
@@ -151,34 +141,6 @@ public class SignatureResource {
         }
 
         return true;
-    }
-
-    @Nullable
-    private Signature getDummySignature() throws IOException {
-        final String documentId = "a4ede2b6-396c-4f71-894c-af4a25a91c52";
-        final String cmdFilename = String.format("signatures/%s.cms", documentId);
-        InputStream cms = SignatureResource
-                .class
-                .getClassLoader()
-                .getResourceAsStream(cmdFilename);
-
-        if (cms != null) {
-            final byte[] bytes = Util.toByteArray(cms);
-            Signature dummy = new Signature();
-            dummy.documentId = documentId;
-            dummy.cms = Base64.getEncoder().encodeToString(bytes);
-            return dummy;
-        }
-
-        return null;
-    }
-
-    @Nullable
-    private SignResponse getDummySigResponse() {
-        SignResponse result = new SignResponse();
-        result.responseId = UUID.fromString("5cdcc7bb-6f9e-44f2-b524-cdd865657729");
-        result.consentURL = "https://ais-sas.swisscom.com/sas/web/tk8f63384b81fd4efeb3f278b86506a65etx/otp?lang=en-us";
-        return null;
     }
 
     public static class SignResponse {
